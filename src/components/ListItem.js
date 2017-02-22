2d6c0e98298a6eff1220a6513d7a65b5a720fa26
Hebert Porto <hebertporto@gmail.com>
2017-02-22 13:55:26 -0300

    novos ajustes de tela, inclusao linearGradient

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
-import { Text, Image } from 'react-native';
+import { Text, Image, View } from 'react-native';
 import { CardSection, Card } from './common';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import LinearGradient from 'react-native-linear-gradient';
+
+const myIcon = (<Icon name="copyright" size={18} color="#717171" />);
+const myIcon2 = (<Icon name="cc" size={18} color="#717171" />);
 
 class ListItem extends Component {
   render() {
@@ -12,13 +17,20 @@ class ListItem extends Component {
               style={styles.imageStyle}
               source={{ uri: image }}
           >
-          <Text>
-          {title}
-          </Text>
+          <LinearGradient
+            colors={['#818181', '#ffffff']}
+            style={styles.linearGradient}>
+              <Text numberOfLines={1} style={styles.titleStyle}>
+              {title}
+              </Text>
+          </LinearGradient>
         </Image>
         </CardSection>
         <CardSection>
-          <Text> {title} </Text>
+          <View style={styles.viewStyle}>
+            <Text numberOfLines={1} style={styles.textStyle}>{myIcon} {title}</Text>
+            <Text numberOfLines={1} style={styles.textStyle}>{myIcon2} {artist}</Text>
+          </View>
         </CardSection>
       </Card>
     );
@@ -27,15 +39,33 @@ class ListItem extends Component {
 
 const styles = {
   titleStyle: {
-    fontSize: 18,
-    paddingLeft: 15
+    fontSize: 20,
+    paddingLeft: 5,
+    color: '#ffffff'
+  },
+  viewStyle: {
+    flex: 1,
+    flexDirection: 'column',
+    paddingLeft: 8,
+    paddingTop: 8,
+    paddingBottom: 8
   },
   imageStyle: {
     flex: 1,
     width: undefined,
-    height: 90,
-    justifyContent: 'center',
-    alignItems: 'center',
+    height: 110,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start'
+  },
+  textStyle: {
+     flex: 1,
+     flexDirection: 'column',
+     paddingBottom: 5
+  },
+  linearGradient: {
+    height: 35,
+    opacity:0.3,
+    alignSelf: 'stretch',
   }
 };
 
--- src/components/common/CardSection.js
+++ src/components/common/CardSection.js
@@ -12,7 +12,7 @@ const CardSection = (props) => {
 const styles = StyleSheet.create({
   containerStyle: {
       borderBottomWidth: 1,
-      padding: 5,
+      // padding: 5,
       backgroundColor: '#fff',
       justifyContent: 'flex-start',
       flexDirection: 'row',
